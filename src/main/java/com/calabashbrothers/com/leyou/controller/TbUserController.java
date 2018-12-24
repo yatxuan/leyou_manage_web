fa88ffe772e5b80d284b4225dff85f2f5e16fedb
YatXuan <YatXuan@163.com>
2018-12-24 19:15:22 +0800

    框架搭建：第一天

--- src/main/java/com/calabashbrothers/com/leyou/controller/TbUserController.java
+++ src/main/java/com/calabashbrothers/com/leyou/controller/TbUserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("TbUser")
 public class TbUserController extends BaseController<TbUser, TbUserServiceImpl> {
 
+    @Override
     @Autowired
     public void setService(TbUserServiceImpl service){
         super.setService(service);
