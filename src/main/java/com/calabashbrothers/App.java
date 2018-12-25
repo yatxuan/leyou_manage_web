fe757aa8493520389a62ec987a1096c04938650d
578754452@qq.com <578754452@qq.com>
2018-12-25 11:17:51 +0800

    修改了basedao

--- src/main/java/com/calabashbrothers/App.java
+++ src/main/java/com/calabashbrothers/App.java
@@ -1,10 +1,10 @@
 package com.calabashbrothers;
 
 import com.calabashbrothers.common.util.IdWorker;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * <p>Description: 描述 </p>
